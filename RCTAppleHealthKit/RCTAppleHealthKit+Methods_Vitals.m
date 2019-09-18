a827ec64e0a1a3f07bf3c93d25aba1bc38fa9fc4
pablo.rico <pablo.rico@strivelabs.io>
2019-09-18 13:15:43 +0200

    Add HRV

--- RCTAppleHealthKit/RCTAppleHealthKit+Methods_Vitals.m
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Vitals.m
@@ -39,6 +39,38 @@
     }];
 }
 
+- (void)vitals_getHeartRateVariabilitySamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
+{
+    HKQuantityType *heartRateVariabilitySDNN = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRateVariabilitySDNN];
+
+    HKUnit *unit = [RCTAppleHealthKit hkUnitFromOptions:input key:@"unit" withDefault:[HKUnit secondUnitWithMetricPrefix:HKMetricPrefixMilli]];
+    NSUInteger limit = [RCTAppleHealthKit uintFromOptions:input key:@"limit" withDefault:HKObjectQueryNoLimit];
+    BOOL ascending = [RCTAppleHealthKit boolFromOptions:input key:@"ascending" withDefault:false];
+    NSDate *startDate = [RCTAppleHealthKit dateFromOptions:input key:@"startDate" withDefault:nil];
+    NSDate *endDate = [RCTAppleHealthKit dateFromOptions:input key:@"endDate" withDefault:[NSDate date]];
+    if(startDate == nil){
+        callback(@[RCTMakeError(@"startDate is required in options", nil, nil)]);
+        return;
+    }
+    NSPredicate * predicate = [RCTAppleHealthKit predicateForSamplesBetweenDates:startDate endDate:endDate];
+
+    [self fetchQuantitySamplesOfType:heartRateVariabilitySDNN
+                                unit:unit
+                           predicate:predicate
+                           ascending:ascending
+                               limit:limit
+                          completion:^(NSArray *results, NSError *error) {
+        if(results){
+            callback(@[[NSNull null], results]);
+            return;
+        } else {
+            NSLog(@"error getting heart rate variability samples: %@", error);
+            callback(@[RCTMakeError(@"error getting heart rate variability samples", nil, nil)]);
+            return;
+        }
+    }];
+}
+
 
 - (void)vitals_getBodyTemperatureSamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
 {
